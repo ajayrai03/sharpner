f5e74cfe8c8cea9a52528effcef05256619f13b9
ajayrai03 <ajayrai1012@gmail.com>
2023-09-21 14:57:02 +0530

    dom intro

---
+++ dom.js
@@ -0,0 +1,17 @@
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title=123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent='Hello';
+// console.log(document.forms[0]);
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 1px #000';
+var addItem=document.getElementsByClassName('title');
+addItem[0].style.fontWeight="bold";
+addItem[0].style.color='green';
