74e5d480ef5951704b557046661ff4fa49c0c470
ajayrai03 <ajayrai1012@gmail.com>
2023-09-21 15:27:43 +0530

    getElementsByTagName intro

--- dom.js
+++ dom.js
@@ -10,14 +10,18 @@
 // console.log(document.all[10]);
 // document.all[10].textContent='Hello';
 // console.log(document.forms[0]);
-var header=document.getElementById('main-header');
-header.style.borderBottom='solid 1px #000';
-var addItem=document.getElementsByClassName('title');
-addItem[0].style.fontWeight="bold";
-addItem[0].style.color='green';
+// var header=document.getElementById('main-header');
+// header.style.borderBottom='solid 1px #000';
+// var addItem=document.getElementsByClassName('title');
+// addItem[0].style.fontWeight="bold";
+// addItem[0].style.color='green';
 
-var items=document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor='green';
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
-}
+// var items=document.getElementsByClassName('list-group-item');
+// items[2].style.backgroundColor='green';
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight='bold';
+// }
+
+
+var li=document.getElementsByTagName('li');
+console.log(li[0].textContent);
